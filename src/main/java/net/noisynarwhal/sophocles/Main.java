78aa224a6fac40afea1ff933b965a793b26510f7
David Liou <david.liou@nih.gov>
2022-06-01 10:15:37 -0400

    Update words lists

--- src/main/java/net/noisynarwhal/sophocles/Main.java
+++ src/main/java/net/noisynarwhal/sophocles/Main.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
